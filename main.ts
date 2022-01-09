96d1b2fcaa373c4794901577f0eb25be737ab6db
junpeko5 <junpeko5@gmail.com>
2022-01-09 13:06:19 +0900

    Working Vite working with Vue

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 import './style.scss';
 import { debounce } from 'lodash-es';
 import styles from `./example.module.css`
+import favicon from './favicon.svg'
 
 console.log(debounce);
 
 document.querySelector('#app').innerHTML = `
   <h1 class="${styles.title}">Hello Vite!!!</h1>
+  <img src="${favicon}" />
+  <img src="/favicon.svg" />
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
