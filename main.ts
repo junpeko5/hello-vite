7a8091ca70123a1f4270fed4384327dee777505e
junpeko5 <junpeko5@gmail.com>
2022-01-09 12:56:56 +0900

    CSS & Debugging

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-import './style.css';
+import './style.scss';
 import { debounce } from 'lodash-es';
+import styles from `./example.module.css`
 
 console.log(debounce);
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!!!</h1>
+  <h1 class="${styles.title}">Hello Vite!!!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
